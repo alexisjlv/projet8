ee2f9d8404463bd3499670685296e65c9f5a3a46
alexisjlv <alexis.jolivel@gmail.com>
2018-01-10 00:05:47 +0100

    correction erreur id -> generation id a la milliseconde de l'action

--- js/store.js
+++ js/store.js
@@ -80,15 +80,6 @@
 
 		callback = callback || function () {};
 
-		// Generate an ID
-	    var newId = ""; 
-	    //var charset = "0123456789";
-        var charset = "012";
-
-        for (var i = 0; i < 6; i++) {
-     		newId += charset.charAt(Math.floor(Math.random() * charset.length));
-		}
-
 		// If an ID was actually given, find the item and update each property
 		if (id) {
 			for (var i = 0; i < todos.length; i++) {
@@ -103,10 +94,11 @@
 			localStorage[this._dbName] = JSON.stringify(data);
 			callback.call(this, todos);
 		} else {
+			// genere un id en fonction de la milliseconde de l'action
+            var objectDate = new Date();
+            var uniqueID =  objectDate.getTime();
 
-    		// Assign an ID
-			updateData.id = parseInt(newId);
-    
+			updateData.id = parseInt(uniqueID);
 
 			todos.push(updateData);
 			localStorage[this._dbName] = JSON.stringify(data);
